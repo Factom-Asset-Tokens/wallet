5036fcde0808fa1408fcae395e52934fc3a6b236
Paul Bernier <bernier.pja@gmail.com>
2019-01-05 15:25:13 +1300

    Improve track token view

--- src/views/TrackToken.vue
+++ src/views/TrackToken.vue
@@ -3,11 +3,11 @@
     <v-layout>
       <v-flex xs12 sm8 offset-sm2>
         <v-card>
-          <v-card-title>
-            <span class="headline">Track an existing token</span>
-          </v-card-title>
-          <v-card-text>
-            <v-form ref="form" v-model="valid" lazy-validation>
+          <v-form ref="form" v-model="valid" @submit="submit" lazy-validation>
+            <v-card-title>
+              <span class="headline">Track an existing token</span>
+            </v-card-title>
+            <v-card-text>
               <v-text-field
                 v-model.trim="tokenChainId"
                 :rules="tokenChainIdRules"
@@ -15,13 +15,14 @@
                 counter="64"
                 len
                 required
+                autofocus
               ></v-text-field>
-            </v-form>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn :disabled="!valid" @click="submit">track</v-btn>
-            <v-btn @click="clear">clear</v-btn>
-          </v-card-actions>
+            </v-card-text>
+            <v-card-actions>
+              <v-btn :disabled="!valid" type="submit">track</v-btn>
+              <v-btn @click="clear">clear</v-btn>
+            </v-card-actions>
+          </v-form>
         </v-card>
       </v-flex>
     </v-layout>
@@ -33,38 +34,50 @@
 </template>
 
 <script>
+const HEX_REGEXP = /^[0-9a-fA-F]+$/;
+
 export default {
   data: () => ({
     snackError: false,
-    snackErrorMessage: '',
+    snackErrorMessage: "",
     valid: true,
     tokenChainId: "",
     tokenChainIdRules: [
       v => !!v || "Token chain ID is required",
-      v => (v && v.length === 64) || "Token chain ID must be 64 characters"
+      v => (v && v.length === 64) || "Token chain ID must be 64 characters",
+      v => (v && HEX_REGEXP.test(v)) || "Invalid characters"
     ]
   }),
 
   methods: {
-    async submit() {
+    async submit(e) {
+      e.preventDefault();
+
       if (this.$refs.form.validate()) {
         const cli = this.$store.getters["fatd/cli"];
         const tokenCli = cli.getTokenCLI(this.tokenChainId);
-        const result = await tokenCli.getIssuance();
 
-        if (result) {
+        try {
+          const result = await tokenCli.getIssuance();
+
           const token = {
             chainId: result.chainid,
             issuer: result.issuerid,
             tokenId: result.tokenid,
             issuance: result.issuance
-          }
+          };
 
           this.$store.commit("tokens/trackToken", token);
           this.$router.push({ path: `/token/${this.tokenChainId}` });
-        } else {
+        } catch (e) {
+          const code = JSON.parse(e.message).code;
+          if (code === -32800) {
+            this.snackErrorMessage = "Token not found.";
+          } else {
+            this.snackErrorMessage = "Unknown error encountered.";
+            console.error(e);
+          }
           this.snackError = true;
-          this.snackErrorMessage = `Failed to retrieve token issuance.`;
         }
       }
     },
