0fee2ef0eb4ad4e3f8ee1d90fc8d87e95082d7d1
Paul Bernier <bernier.pja@gmail.com>
2019-12-11 09:24:16 -0800

    feat: add info message when importing keys

--- src/components/Factoid/Addresses/AddressImportDialog.vue
+++ src/components/Factoid/Addresses/AddressImportDialog.vue
@@ -1,34 +1,38 @@
 <template>
   <v-dialog v-model="display" lazy max-width="650px" @keydown.esc="display = false">
     <v-card>
-      <v-card-title>
+      <v-card-title class="headline primary white--text" primary-title>
         <span class="headline">Import Address</span>
       </v-card-title>
       <v-card-text>
-        <v-container>
-          <v-form v-model="valid" ref="form">
-            <v-layout wrap>
-              <v-flex xs12>
-                <v-text-field
-                  v-model="address"
-                  :append-icon="showAddress ? 'visibility_off' : 'visibility'"
-                  :type="showAddress ? 'text' : 'password'"
-                  @click:append="showAddress = !showAddress"
-                  :rules="addressRules"
-                  label="Private FCT or EC address"
-                  counter="52"
-                  ref="addressInput"
-                  autofocus
-                  box
-                  required
-                ></v-text-field>
-              </v-flex>
-              <v-flex xs12>
-                <v-text-field v-model.trim="name" label="Name (Optional)" box></v-text-field>
-              </v-flex>
-            </v-layout>
-          </v-form>
-        </v-container>
+        <v-form v-model="valid" ref="form">
+          <v-layout wrap>
+            <v-flex xs12>
+              <v-text-field
+                v-model="address"
+                :append-icon="showAddress ? 'visibility_off' : 'visibility'"
+                :type="showAddress ? 'text' : 'password'"
+                @click:append="showAddress = !showAddress"
+                :rules="addressRules"
+                label="Private FCT or EC address"
+                counter="52"
+                ref="addressInput"
+                autofocus
+                box
+                required
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-text-field v-model.trim="name" label="Name (Optional)" box></v-text-field>
+            </v-flex>
+            <v-flex xs12>
+              <v-alert :value="true" icon="info" color="primary" outline
+                >Note that manually imported addresses are not backed up by your Recovery Phrase. It is strongly
+                suggested to do a full encrypted backup after every import by going to Settings > Backup</v-alert
+              >
+            </v-flex>
+          </v-layout>
+        </v-form>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
